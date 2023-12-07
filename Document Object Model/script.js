7739af9fc33807219776fcad7ac7a06b2a1d21f5
danCode-s <dancodes9@gmail.com>
2023-12-07 19:38:46 +0500

    improved functionality

--- Document Object Model/script.js
+++ Document Object Model/script.js
@@ -14,10 +14,16 @@ function subscribe(){
 function calculateTotal(){
     let orderCost = Number(document.querySelector('.cost-input').value);
     orderCost *= 100;
-
-    if (orderCost <= 4000){
+    
+    if(orderCost < 0){
+        document.querySelector('.js-total-cost').classList.add('js-error-red');
+        document.querySelector('.js-error-red').innerHTML = 'Error: Cost cannot be less than $0';
+        
+    } else if (orderCost <= 4000){
+        document.querySelector('.js-total-cost').classList.remove('js-error-red');
         document.querySelector('.js-total-cost').innerHTML = `$${((orderCost + 1000) / 100 )}`;
     } else {
+        document.querySelector('.js-total-cost').classList.remove('js-error-red');
         document.querySelector('.js-total-cost').innerHTML = `$${orderCost / 100}`;
     }
 }
