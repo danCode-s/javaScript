4760e4229f5cefe8e5fbe66ae7a664bc1302a124
danCode-s <dancodes9@gmail.com>
2023-12-07 19:29:57 +0500

    button functionality

--- Exercise 10/script.js
+++ Exercise 10/script.js
@@ -1,11 +1,21 @@
 
-function buttonClickColor(class_1){
-    const buttons = document.querySelector(class_1);
-
-    if (buttons.classList.contains('is-toggled')){
-        buttons.classList.remove('is-toggled');
-    }else {
-        buttons.classList.add('is-toggled');
+function buttonClickColor(class_1) {
+    const button = document.querySelector(class_1);
+    if (!button.classList.contains('is-toggled')) {
+  
+      // Before turning this button ON, check if there's
+      // already a button that's turned ON and turn it OFF.
+      turnOffPreviousButton();
+  
+      button.classList.add('is-toggled');
+    } else {
+      button.classList.remove('is-toggled');
     }
-    
+  }
+  
+  function turnOffPreviousButton() {
+    const previousButton = document.querySelector('.is-toggled');
+    if (previousButton) {
+      previousButton.classList.remove('is-toggled');
+    }
 }
