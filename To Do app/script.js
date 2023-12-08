8737609978d8251ee845525fa31636658e4fdf2e
danCode-s <dancodes9@gmail.com>
2023-12-08 12:45:00 +0500

    Basic Functionality

--- To Do app/script.js
+++ To Do app/script.js
@@ -2,11 +2,19 @@ const toDoList = [];
 function takeToDo(){
     const inputElement = document.querySelector('.js-input');
     const name = inputElement.value;
-    console.log(toDoList);
     toDoList.push(name);
 
     inputElement.value = '';
 
+    
+    let finalToDo = '';
+    for (let i = 0; i < toDoList.length; i++){
+        console.log(i);
+        const list = `<p>${toDoList[i]}</p>`;
+        finalToDo += list;
+    }
+
     document.querySelector('.js-output')
-    .innerHTML = `${toDoList}`;
+        .innerHTML = finalToDo;
 }
+    
