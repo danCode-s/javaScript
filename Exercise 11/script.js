3400cfe67b0f30057f8df631dea969ae6230a0c7
danCode-s <dancodes9@gmail.com>
2023-12-08 13:49:08 +0500

    Array Exercises

---
+++ Exercise 11/script.js
@@ -0,0 +1,77 @@
+function getLastValue(array){
+    const lastValue = array.length - 1;
+    console.log(array[lastValue]);
+}
+
+getLastValue([2,3,43,32]);
+
+function arraySwap(array){
+    const firstValue = array[0];
+    const lastValue = array[array.length - 1];
+
+    array[0] = lastValue;
+    array[array.length - 1] = firstValue;
+
+    return array;
+}
+
+console.log(arraySwap([1, 2, 323, 434, 4]));
+
+
+function addNum(arr_1, num){
+    let newArr = [];
+    for (let i = 0; i < arr_1.length; i++){
+        newArr.push(arr_1[i] + num);
+    }
+    return newArr;
+}
+
+console.log(addNum([1, 2, 3], 2));
+
+function addArrays(arr_1, arr_2){
+    let newArr = [];
+    for (let i = 0; i < arr_1.length; i++){
+        newArr.push(arr_1[i] + arr_2[i]);
+    }
+    return newArr;
+}
+console.log(addArrays([1, 2, 3], [1, 1, 3]));
+
+function minMax(num){
+    let result = {
+        min: null,
+        max: null
+    };
+    for (let i = 0; i < num.length; i++){
+        const value = num[i]
+        if (result.min === null || value < result.min) {
+            result.min = value;
+          }
+
+          if (result.max === null || value > result.max) {
+            result.max = value;
+          }
+    }
+
+    return result;
+}
+
+console.log(minMax([]));
+
+function countWords(words) {
+    const result = {};
+
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+
+      if (!result[word]) {
+        result[word] = 1;
+      } else {
+        result[word]++;
+      }
+    }
+
+    return result;
+}
+
+console.log(countWords(['apple', 'grape', 'apple', 'apple']));
