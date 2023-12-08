3712a1a50dac2d20e1f1ad768a80fe90feb15cc2
danCode-s <dancodes9@gmail.com>
2023-12-08 18:11:20 +0500

    better functionality

--- To Do app/script.js
+++ To Do app/script.js
@@ -1,16 +1,31 @@
 const toDoList = [];
+
+renderToDo();
+
 function takeToDo(){
     const inputElement = document.querySelector('.js-input');
     const name = inputElement.value;
     toDoList.push(name);
 
     inputElement.value = '';
+    renderToDo();
 
     
+}
+function renderToDo(){
+
     let finalToDo = '';
     for (let i = 0; i < toDoList.length; i++){
         console.log(i);
-        const list = `<p>${toDoList[i]}</p>`;
+        const list = `
+        <p>${toDoList[i]}
+            <button onclick="
+            toDoList.splice(${i}, 1);
+            renderToDo();
+            ">
+            Delete</button>
+        </p>
+        `;
         finalToDo += list;
     }
 
