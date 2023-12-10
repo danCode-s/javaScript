0ba970ac150473905278bab3d7e62b0549bc208c
danCode-s <dancodes9@gmail.com>
2023-12-10 14:15:42 +0500

    uses eventListeners

--- To Do app/script.js
+++ To Do app/script.js
@@ -7,41 +7,41 @@ renderToDo();
 
 function takeToDo(){
     saveList();
-
+    
     
     const inputElement = document.querySelector('.js-input');
     const dateElement = document.querySelector('.js-due-date');
     const name = inputElement.value;
     const dueDate = dateElement.value;
-
+    
     toDoList.push({
         name,
         dueDate,
     });
-
-
+    
+    
     getList();
-
-
+    
+    
     inputElement.value = '';
     
     renderToDo();
-
+    
 }
-function renderToDo(){
 
+
+function renderToDo(){
+    
     let finalToDo = '';
+
     toDoList.forEach((todoObject, index) => {
         const { name, dueDate } = todoObject;
 
         const list = `
         <div>${name}</div>
         <div>${dueDate}</div>
-            <button onclick="
-            toDoList.splice(${index}, 1);
-            renderToDo();
-            getList();
-            " class="red-btn">
+            <button
+            class="red-btn delete-btn">
             Delete</button>
         
         `;
@@ -51,8 +51,28 @@ function renderToDo(){
     document.querySelector('.js-output')
         .innerHTML = finalToDo;
     
+
+
+    document.querySelectorAll('.delete-btn')
+        .forEach((deleteButton, index) => {
+            deleteButton.addEventListener('click', () => {
+                toDoList.splice(index, 1);
+                renderToDo();
+                getList();
+                
+            });
+
+        });
+    
         
 }
+document.querySelector('.add-btn')
+.addEventListener('click', () => {
+    takeToDo();
+});
+
+
+    
 
 function saveList(){
     toDoList = JSON.parse(localStorage.getItem('list'));
